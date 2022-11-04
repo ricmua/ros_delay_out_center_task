25366b7a8fcf295753f22ed5635ac003a4ee7f62
ricmua <ricmua@whit.contact>
2022-11-04 13:38:23 -0400

    Added delay_out_center_task Python package as a submodule to be installed during the ROS2 package build. Builds and loads.

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ package_name = 'ros_delay_out_center_task'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=[package_name],
+    version='0.0.1',
+    packages=[package_name, 'delay_out_center_task'],
+    package_dir={'delay_out_center_task': 'delay_out_center_task/delay_out_center_task'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -17,6 +18,7 @@ setup(
     maintainer_email='nml@whit.contact',
     description='TODO: Package description',
     license='Mozilla Public License 2.0',
+    requires=['delay_out_center_task'],
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
